be99f2cdf537aab22754fecc20c6aea451f440c0
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-05-17 21:42:57 +0600

    added array bins

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SortingVisualizer from './components/SortingVisualizer';
+import './App.css';
 
 class App extends React.Component {
 	render() {
--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class SortingVisualizer extends React.Component {
+	state = {
+		array: [],
+	};
+
+	componentDidMount() {
+		this.constructArray();
+	}
+
+	getRandomInt = (min, max) => {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	constructArray = () => {
+		const arr = [];
+		for (let i = 0; i < 100; i++) {
+			arr.push(this.getRandomInt(10, 300));
+		}
+		this.setState({ array: arr });
+	};
+
 	render() {
-		return <div>Hello World</div>;
+		const { array } = this.state;
+		return (
+			<div className='container'>
+				<div className='bins'>
+					{array.map((num, index) => (
+						<div
+							className='bin'
+							key={index}
+							style={{ height: `${num}px` }}
+						/>
+					))}
+				</div>
+				<div className='panel'>
+					<button onClick={this.constructArray}>
+						Generate New Array
+					</button>
+				</div>
+			</div>
+		);
 	}
 }
 
